591812c0e1e6923e14194782b18206fb2de3c754
Alex Thiga <loydeflacko@gmail.com>
2022-06-11 11:04:14 +0300

    commit tree b6df138eddbcd892b0b13a09d836c4e047c40f81

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat ()
+{
+    const appendCat = cats.push("Ralph");
+    return appendCat
+}
+function destructivelyPrependCat ( )
+{
+    const prependCat = cats.unshift( 'Bob' );
+    return prependCat
+}
+function destructivelyRemoveLastCat ()
+{
+    const removeLastCat = cats.pop()
+}
+function destructivelyRemoveFirstCat ()
+{
+    const removeCat = cats.shift();
+    return removeCat
+}
+function appendCat ()
+{
+    const newCats = [ ...cats, 'Broom' ]
+    return newCats
+}
+function prependCat ()
+{
+    const catPrepend = [ 'Arnold', ...cats ];
+    return catPrepend
+}
+function removeLastCat ()
+{
+    const catRemove = cats.slice( 0, 2 );
+    return catRemove
+}
+function removeFirstCat ()
+{
+    const removeFirst = cats.slice( 1 );
+    return removeFirst
+}
